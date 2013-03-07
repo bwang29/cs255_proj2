e16f63a70cb2eadeb28ad875f3910bb363be9cb0
Borui Wang <borui@stanford.edu>
2013-03-06 22:05:56 -0800

    certificate chain issue

--- src/borui_keygen_command.bash
+++ src/borui_keygen_command.bash
@@ -1,4 +1,4 @@
 keytool -genkeypair -dname "CN=Very Secure Software, OU=Very Secure Software Group, O=Very Secure Inc, L=Cupertino, 
-S=California, C=US" -alias mykey -keypass foobar -keystore ./keystore -storepass foobar -validity 365 -sigalg "MD5withRSA"
+S=California, C=US" -alias mykey -keypass foobar -keystore ./keystore -storepass foobar -validity 365
 
 # -list -alias mykey -keystore ./keystore -v
